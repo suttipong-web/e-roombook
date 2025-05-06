9ccef858b233072a2c0a04af3cfd07dff50f8cb2
Suttipong <suttipong.r@cmu.ac.th>
2025-05-06 13:33:24 +0700

    Add Link booking admn page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -29,6 +29,8 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800"> Dashboard : รายการจองห้องรายวัน </h1>
+        <a href="/booking/2/ห้องเรียน" target="_blank" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-plus fa-sm text-white-50"></i> ทำรายจองห้อง</a>
     </div>
     <!-- Main row -->
     <div class="row">
